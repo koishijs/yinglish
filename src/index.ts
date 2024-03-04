54c1f4c02d7072c22cb5511e3e93262bdf3c9336
42 <42@koishi.chat>
2024-03-05 05:05:03 +0800

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Context, Schema, Service } from 'koishi'
+import { Context, Schema, Service, h } from 'koishi'
 import { } from 'koishi-plugin-jieba'
 
 
@@ -10,54 +10,35 @@ declare module 'koishi' {
 
 export class Yinglish extends Service {
   lewdnessLevel: number
-  public inject = ['jieba'] as const
   constructor(ctx: Context, config: Yinglish.Config) {
     super(ctx, 'yinglish', true)
     this.lewdnessLevel = config.lewdnessLevel
-    ctx.inject(['jieba', 'yinglish'], (ctx) => {
-      ctx
-        .command('yinglish <text>')
-        .option('lewdness', '-l <lewdness>')
-        .action(async ({ options }, input) => {
-          return ctx.yinglish.chs2yin(input, options.lewdness)
-        })
-    })
+    ctx
+      .command('yinglish <text>')
+      .option('lewdness', '-l <lewdness>')
+      .action(async ({ options }, input) => {
+        return h.text(this.chs2yin(input, options.lewdness))
+      })
   }
 
   _converter(x: string, y: string, lewdness: number): string {
-    if (Math.random() > lewdness) {
-      return x
-    }
-    if (x === '，' || x === '。') {
-      return '……'
-    }
-    if (x === '!' || x === '！') {
-      return '❤'
-    }
-    if (x.length > 1 && Math.random() < this.lewdnessLevel) {
-      return `${x[0]}……${x}`
-    } else {
-      if (y === 'n' && Math.random() < this.lewdnessLevel) {
-        x = '〇'.repeat(x.length)
-      }
-      return `……${x}`
-    }
+    if (Math.random() > lewdness) return x
+    if (x === '，' || x === '。') return '……'
+    if (x === '!' || x === '！') return '❤'
+    if (x.length > 1 && Math.random() < lewdness) return `${x[0]}……${x}`
+    else if (y === 'n' && Math.random() < lewdness) x = '〇'.repeat(x.length)
+    return `……${x}`
   }
 
   chs2yin(text: string, lewdness: number = this.lewdnessLevel) {
-    if (!text) return ''
+    if (!text || lewdness === 0) return ''
     return this.ctx.jieba.tag(text).map(({ word, tag }) => this._converter(word, tag, lewdness)).join('')
   }
 }
 
 export namespace Yinglish {
-  export const name = 'yinglish'
   export const inject = ['jieba'] as const
-
-  export interface Config {
-    lewdnessLevel: number
-  }
-
+  export interface Config { lewdnessLevel: number }
   export const Config: Schema<Config> = Schema.object({
     lewdnessLevel: Schema.number().max(1).min(0).default(0.5).description('默认的淫乱程度').role('slider')
   })
